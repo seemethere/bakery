b94b5051213cde30a9f70fc4f8e303a94a4c8b4f
ian.uriegas <ianuriegas@gmail.com>
2026-05-22 14:46:23 +0000

    fix: polish mobile PWA standalone layout

--- apps/web/src/components/ui/sidebar.tsx
+++ apps/web/src/components/ui/sidebar.tsx
@@ -199,7 +199,7 @@ function Sidebar({
             <SheetTitle>Sidebar</SheetTitle>
             <SheetDescription>Displays the mobile sidebar.</SheetDescription>
           </SheetHeader>
-          <div className="flex h-full w-full flex-col">{children}</div>
+          <div className="flex min-h-0 w-full flex-1 flex-col">{children}</div>
         </SheetContent>
       </Sheet>
     )
